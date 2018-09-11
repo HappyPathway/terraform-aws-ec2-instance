bf304ab00f99424349f1a3a1f1da1025a19b2d2c
Dave Arnold <darnold@hashicorp.com>
2018-09-11 15:49:14 -0700

    updating outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,15 @@
 output "instance_id" {
   value = "${aws_instance.web.id}"
 }
+
+output "public_ip_address" {
+  value = "${aws_instance.web.public_ip}"
+}
+
+output "key_name" {
+  value = "${aws_instance.web.key_name}"
+}
+
+output "availability_zone" {
+  value = "${aws_instance.web.availability_zone}"
+}
