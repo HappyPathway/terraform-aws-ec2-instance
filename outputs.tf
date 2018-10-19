06bd7e88e45732bcb4d9c7e340f11f1d9da87cd0
David Arnold <dja+djaboxx@sonic.net>
2018-10-18 19:37:55 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,20 @@
 output "instance_id" {
-  value = "${element(aws_instance.web.id, 0)}"
+  value = "${aws_instance.web.id}"
 }
 
-output "instance_ids" {
-  value = "${aws_instance.web.*.id}"
-  }
+
 
 
 output "public_ip_address" {
-  value = "${element(aws_instance.web.public_ip, 0)}"
+  value = "${aws_instance.web.public_ip}"
 }
 
-output "public_ip_addresses" {
-  value = "${aws_instance.web.*.public_ip}"
-}
+
 
 output "key_name" {
-  value = "${element(aws_instance.web.key_name, 0)}"
+  value = "${aws_instance.web.key_name}"
 }
 
 output "availability_zone" {
-  value = "${element(aws_instance.web.availability_zone, 0)}"
+  value = "${aws_instance.web.availability_zone}"
 }
