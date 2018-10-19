ada1d01e4f4e64130d3259feb4d30b7eebc9dace
David Arnold <dja+djaboxx@sonic.net>
2018-10-18 19:38:19 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,6 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "web" {
-  count = "${var.instance_count}"
   ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "${var.instance_type}"
   key_name = "${var.key_name}"
