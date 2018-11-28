44b40ceb72fed8112038b7d8f775569bc6cb441a
David Arnold <dja+djaboxx@sonic.net>
2018-11-28 12:05:29 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,6 +17,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "web" {
   ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "${var.instance_type}"
+  subnet_id     = "${var.subnet_id}"
   key_name = "${var.key_name}"
   tags {
     Name = "${var.instance_name}"
